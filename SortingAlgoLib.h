bdc6563e9ecc1b5aaeef906c397d18dd660f6ecd
jiya9113 <jiyam1800@gmail.com>
2023-09-02 20:50:13 +0530

    adding static library

---
+++ SortingAlgoLib.h
@@ -0,0 +1,7 @@
+void bubbleSort(int arr[], int n);
+void merge(int arr[], int start, int mid, int end);
+void mergeSort(int arr[], int start, int end);
+int partition(int arr[], int start, int end);
+void quickSort(int arr[], int start, int end);
+void insertionSort(int arr[], int n);
+void selectionSort(int arr[],int n);
