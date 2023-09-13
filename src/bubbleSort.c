f2f3e2de9235e22de453dc6960f05a962f66a5d0
jiya9113 <jiyam1800@gmail.com>
2023-09-13 15:38:30 +0530

    bubbleSort

--- src/bubbleSort.c
+++ src/bubbleSort.c
@@ -2,12 +2,19 @@
 
 void bubbleSort(int arr[], int n){
     for(int i=0;i<n-1;i++){
+        int swap=0;
         for(int j=0;j<n-i-1;j++){
+            // Swap the elements if larger element occurs before smaller element
             if(arr[j]>arr[j+1]){
                 int temp=arr[j];
                 arr[j]=arr[j+1];
                 arr[j+1]=temp;
+                swap++;
             }
+        }
+        // if there are no swaps then array is sorted
+        if(swap==0){
+            return;
         }
     }
 }
