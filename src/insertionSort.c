f0363ea7e59ac31bbd032a0e8d33622701b7cf34
jiya9113 <jiyam1800@gmail.com>
2023-09-13 15:40:16 +0530

    insertionSort

--- src/insertionSort.c
+++ src/insertionSort.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 
 void insertionSort(int arr[], int n){
-    int i,key,j;
-    for(i=0;i<n;i++){
-        key=arr[i];
-        j=i-1;
-        while(j>=0 && arr[j]>key){
+    for(int i=1;i<n;i++){
+        int ele=arr[i];
+        int j=i-1;
+        // Move element by one position to right if the are greater tha current element
+        while(j>=0 && arr[j]>ele){
             arr[j+1]=arr[j];
             j--;
         }
-        arr[j+1]=key;
+        // Place the element at it,s position
+        arr[j+1]=ele;
     }
 }
