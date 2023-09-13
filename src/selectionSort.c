6c86ab5b31a875d6e4a1fb2b5c36aa51fb5f582b
jiya9113 <jiyam1800@gmail.com>
2023-09-13 15:45:20 +0530

    selectionSort

--- src/selectionSort.c
+++ src/selectionSort.c
@@ -2,12 +2,15 @@
 
 void selectionSort(int arr[], int n){
     for(int i=0;i<n;i++){
+        // index of minimum element
         int min=i;
+        // Finding the minimum element in array
         for(int j=i+1;j<n;j++){
             if(arr[min]>arr[j]){
                 min=j;
             }
         }
+        // Swapping the found minimum element with current index
         if(min!=i){
             int temp=arr[i];
             arr[i]=arr[min];
